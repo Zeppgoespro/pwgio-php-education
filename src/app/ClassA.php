f3073972966efe9123b6bae0710c44de1572bed3
Zepp <zeppgoesblack@gmail.com>
2023-04-18 22:25:51 +0500

    vid 2.15 - Anonymous Classes

--- src/app/ClassA.php
+++ src/app/ClassA.php
@@ -4,16 +4,57 @@ namespace App;
 
 class ClassA
 {
-  protected static string $name = 'A';
+  // protected static string $name = 'A';
 
-  public function getName(): string
+  // public function getName(): string
+  // {
+  //   #var_dump(get_called_class());
+  //   return static::$name;
+  // }
+
+  // public static function make(): static
+  // {
+  //   return new static();
+  // }
+
+  public function __construct(public int $x, public int $y)
   {
-    #var_dump(get_called_class());
-    return static::$name;
+
   }
 
-  public static function make(): static
+  public function foo()
   {
-    return new static();
+    return 'foo';
   }
+
+  public function bar(): object
+  {
+    // return new class($this->x, $this->y) extends classA
+    // {
+    //   public function __construct(public int $x, public int $y)
+    //   {
+    //     parent::__construct($x, $y);
+
+    //     echo $this->foo();
+    //   }
+    // };
+
+    // return new class($this->x, $this->y)
+    // {
+    //   public function __construct(public int $x, public int $y)
+    //   {
+    //     var_dump($x, $y);
+    //   }
+    // };
+
+    return new class ($this)
+    {
+      public function __construct(ClassA $myObj)
+      {
+        var_dump($myObj);
+      }
+    };
+
+  }
+
 }
--- src/public/index.php
+++ src/public/index.php
@@ -1,9 +1,28 @@
 <?php
 
-use App\Customer;
-
 require_once __DIR__ . '/../vendor/autoload.php';
 
-$customer = new Customer();
+use App\ClassA;
+use App\MyInterface;
 
-$customer->updateProfile();
+// $obj = new class(1, 2, 3) implements MyInterface #extends MyClass
+// {
+
+//   #use MyTrait;
+
+//   public function __construct(public int $x, public int $y, public int $z)
+//   {
+
+//   }
+
+// };
+
+// foo($obj);
+
+// function foo(MyInterface $obj) {
+//   var_dump($obj);
+// }
+
+$obj = new ClassA(1,2);
+
+var_dump($obj->bar());
