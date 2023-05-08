5dfd3eb30417d57c82b947393f85d57b93a16734
Zepp <zeppgoesblack@gmail.com>
2023-05-09 00:57:45 +0500

    vid 2.30 - PHP PDO Tutorial Part 1 - Prepared Statements - SQL Injection

--- src/app/Controllers/HomeController.php
+++ src/app/Controllers/HomeController.php
@@ -3,38 +3,46 @@
 declare(strict_types=1);
 
 namespace App\Controllers;
+
 use App\View;
+use PDO;
 
 class HomeController
 {
 
   public function index(): View
   {
-    # return (new View('index'))->render();
+    try {
 
-    # return View::make('index')->render();
+      $db = new PDO('mysql:host=database;dbname=my_db', 'root', 'yesenin', [
 
-    #return (string) View::make('index');
+      ]);
+
+      $query = 'SELECT * FROM `users`';
+
+      /*
+      $stmt = $db->query($query);
+
+      echo '<pre>';
+      var_dump($stmt->fetchAll());
+      echo '</pre>';
+      */
+
+      foreach ($db->query($query) as $user):
+
+        echo '<pre>';
+        var_dump($user);
+        echo '</pre>';
+
+      endforeach;
+
+    } catch (\PDOException $e) {
+      throw new \PDOException($e->getMessage(), $e->getCode());
+    }
+
+    # var_dump($db);
 
     return View::make('index');
-  }
-
-  public function download()
-  {
-    header('Content-Type: image/jpeg');
-    header('Content-Disposition: attachment; filename="buba.jpg"');
-
-    readfile(STORAGE_PATH . '/1-life.jpg');
-  }
-
-  public function upload()
-  {
-    $filePath = STORAGE_PATH . '/' . $_FILES['receipt']['name'];
-
-    move_uploaded_file($_FILES['receipt']['tmp_name'], $filePath);
-
-    header('Location: /');
-    exit;
   }
 
 }
